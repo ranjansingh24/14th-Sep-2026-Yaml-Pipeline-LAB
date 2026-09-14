db9ce3c8153803bcbe0d1da94fc9131cf04fecbc
ranjansingh24 <singhranjan.net@gmail.com>
2026-09-14 12:50:03 +0530

    Add default empty maps in variable.tf and pass absolute var-file path in azure-pipelines.yml

--- Parent-Moudle/variable.tf
+++ Parent-Moudle/variable.tf
@@ -1,8 +1,8 @@
-variable "p-rgs" {}
-variable "p-vnet" {}
-variable "p-sbn" {}
-variable "p-pip" {}
-variable "p-bas" {}
-variable "p-nsg" {}
-variable "p-nic" {}
+variable "p-rgs" { default = {} }
+variable "p-vnet" { default = {} }
+variable "p-sbn" { default = {} }
+variable "p-pip" { default = {} }
+variable "p-bas" { default = {} }
+variable "p-nsg" { default = {} }
+variable "p-nic" { default = {} }
 variable "p-vm" { default = {} }
