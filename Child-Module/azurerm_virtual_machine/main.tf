7eb7da33ca57aa514212feb2d2579d09401f3c56
ranjansingh24 <singhranjan.net@gmail.com>
2026-09-14 15:41:22 +0530

    Update resource location to East US 2 to resolve region SKU quota limits

--- Child-Module/azurerm_virtual_machine/main.tf
+++ Child-Module/azurerm_virtual_machine/main.tf
@@ -3,11 +3,12 @@ resource "azurerm_linux_virtual_machine" "vm-chapra" {
   name                            = each.value.name
   resource_group_name             = each.value.group
   location                        = each.value.location
-  size                            = lookup(each.value, "size", "Standard_B2s")
+  size                            = lookup(each.value, "size", "Standard_B1s")
   admin_username                  = "adminranjan"
   admin_password                  = "P@ssw0rd123456!"
   disable_password_authentication = false
   network_interface_ids           = each.value.network_interface_ids
+  zone                            = "1"
 
   os_disk {
     caching              = "ReadWrite"
